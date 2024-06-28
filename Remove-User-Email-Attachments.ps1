837b4436b6e7bfcc42c2a6c05a1e5cd965c39c58
DylanWoodhead <158052428+DylanWoodhead@users.noreply.github.com>
2024-06-28 10:19:58 +0100

    Updating script with graph cmdlets where possible and not invoke-restmethod

--- Remove-User-Email-Attachments.ps1
+++ Remove-User-Email-Attachments.ps1
@@ -1,15 +1,25 @@
+### Variables to change
 # User UPN of mailbox to clear
-$userPrincipalName = "rhys.lavender@propelfinance.co.uk"
+$userPrincipalName = ""
+
+# Amount of months to look back for emails
+$months = 24
+
+# Set the minimum attachment size to process (in MB)
+$minAttachmentSize = 0
+
+### ONLY change if needed
+# Set the Microsoft List name
+$listName = "IT-Email-Attachments-Removed-'$userPrincipalName'"
+
+### Do not change anything below this line
+
+# Set the date range to # months ago and before
+$dateRange = (Get-Date).AddMonths(-$months).ToString("yyyy-MM-ddTHH:mm:ssZ")
+$dateFilter = "createdDateTime le $dateRange"
 
 # Set the maximum number of emails to process per folder
 $maxEmailsPerFolder = 500
-
-# Set the date range to # months ago and before
-$dateRange = (Get-Date).AddMonths(-36).ToString("yyyy-MM-ddTHH:mm:ssZ")
-$dateFilter = "createdDateTime le $dateRange"
-
-# Set the minimum attachment size to process (in MB)
-$minAttachmentSize = 3
 
 # Authenticate to Azure
 try
@@ -24,16 +34,16 @@ catch {
 }
 
 # Set your Azure App registration details
-$ApplicationID = "f7a0dfc4-17ec-4229-91c3-3994f0d9429f"
-$ClientSecret = Get-AzKeyVaultSecret -VaultName "IT-Access" -Name "Graph-API-Removing-Email-Attachments" -AsPlainText
-$TenantID = "3e833290-3714-42e2-a81f-a6606b883185"
+$ApplicationID = ""
+$ClientSecret = ""
+$TenantID = ""
 
 try{
     $graphtokenBody = @{   
     Grant_Type    = "client_credentials"   
     Scope         = "https://graph.microsoft.com/.default"   
     Client_Id     = $ApplicationID   
-    Client_Secret = $ClientSecret
+    Client_Secret = $ClientSecret.SecretValue | ConvertFrom-SecureString -AsPlainText
     }  
 
     $graphToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token" -Method POST -Body $graphtokenBody | Select-Object -ExpandProperty Access_Token 
@@ -48,122 +58,130 @@ try{
 }
 
 # Get the user's mailbox folders
-$headers = @{
-    "Authorization" = "Bearer $graphtoken"
+$userMailboxFolders = Get-MgUserMailFolder -UserId $userPrincipalName
+
+$displayName = $userPrincipalName.Replace("@email.co.uk","")
+$displayName = $displayName.Replace("."," ")
+try {
+    $getUserSiteId = (Get-MgSite -All | Where {$_.DisplayName -like $displayName} | Select Id).Id
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+    exit
 }
 
-$userMailboxFolders = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userPrincipalName/mailFolders" -Headers $headers
-
-$getUserSiteId = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userPrincipalName/drive/SharePointIds" -Headers $headers
+$siteId = ($getUserSiteId -split ',')[1]
 
 # Date of script run for output file
 $todaysDate = (Get-Date).ToString("yyyy-MM-ddTHH:mm:ssZ")
 
-# Set the Microsoft List information
-$listName = "IT-Email-Attachments-Removed-'$userPrincipalName'"
-$siteId = $getUserSiteId.siteId
-
 # Check if the Microsoft List exists, and create it if it doesn't
-$existingLists = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/lists" -Headers $headers
-if ($existingLists.Value.displayName -notcontains $listName) {
-$newList = [PSCustomObject]@{
-    displayName = $listName
-    columns = @(
-        [PSCustomObject]@{
-            name = "Subject"
-            text = [PSCustomObject]@{}
-        },
-        [PSCustomObject]@{
-            name = "AttachmentName"
-            text = [PSCustomObject]@{}
-        },
-        [PSCustomObject]@{
-            name = "AttachmentSizeMB"
-            number = [PSCustomObject]@{}
-        },
-        [PSCustomObject]@{
-            name = "EmailCreatedDate"
-            text = [PSCustomObject]@{}
-        },
-        [PSCustomObject]@{
-            name = "FolderName"
-            text = [PSCustomObject]@{}
-        },
-        [PSCustomObject]@{
-            name = "EmailURL"
-            text = [PSCustomObject]@{}
+$existingLists = Get-MgSiteList -SiteId $siteId
+if ($existingLists.displayName -notcontains $listName) {
+    $newList = [PSCustomObject]@{
+        displayName = $listName
+        columns = @(
+            [PSCustomObject]@{
+                name = "Subject"
+                text = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "AttachmentName"
+                text = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "AttachmentSizeMB"
+                number = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "EmailCreatedDate"
+                text = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "FolderName"
+                text = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "EmailURL"
+                text = [PSCustomObject]@{}
+            },
+            [PSCustomObject]@{
+                name = "ScriptRunDate"
+                text = [PSCustomObject]@{}
+            }
+        )
+        list = [PSCustomObject]@{
+            template = "genericList"
         }
-    )
-    list = [PSCustomObject]@{
-        template = "genericList"
     }
-}
-    $newListJson = $newList | ConvertTo-Json -Depth 3
-    $newListId = Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/lists" -Body $newListJson -Headers $headers -ContentType "application/json"
-} else {
-    Write-Output "List already made"
-    $newListId = $existingLists.Value | Where-Object { $_.displayName -eq $listName }
-}
+        $newListJson = $newList | ConvertTo-Json -Depth 3
+        # Using Invoke as New-MgSiteList doesn't work
+        $newListId = Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/lists" -Body $newListJson -ContentType "application/json"
+    } else {
+        Write-Output "List already made"
+        $newListId = $existingLists | Where-Object { $_.displayName -eq $listName }
+    }
 
-$listURL = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/lists/$($newListId.id)" -Body ($listItemBody | ConvertTo-Json) -Headers $headers -ContentType "application/json"
-$listURL.webUrl
+$listURL = Get-MgSiteList -SiteId $siteId -ListId $newListId.Id
+$listURL.WebUrl
 
 $ErrorActionPreference = "Stop"
 
 # Iterate through the user's mailbox folders and remove attachments
-foreach ($folder in $userMailboxFolders.Value) {
-    Write-Output "-- $($folder.displayName) --"
+foreach ($folder in $userMailboxFolders) {
+    Write-Output "-- $($folder.DisplayName) --"
 
     $skipCount = 0
     $totalMessages = 0
 
     while ($true) {
-        $messages = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userPrincipalName/mailFolders/$($folder.Id)/messages`?`$top=$maxEmailsPerFolder&`$skip=$skipCount&`$filter=$dateFilter" -Headers $headers
-        $totalMessages += $messages.Value.Count
+        $messages = Get-MgUserMailFolderMessage -UserId $userPrincipalName -MailFolderId $folder.Id -Top $maxEmailsPerFolder -Skip $skipCount -Filter $dateFilter
+        $totalMessages += $messages.Count
 
-        foreach ($message in $messages.Value) {
+        foreach ($message in $messages) {
             if ($message.HasAttachments) {
 
-                $attachments = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userPrincipalName/mailFolders/$($folder.Id)/messages/$($message.Id)/attachments" -Headers $headers
+                $attachments = Get-MgUserMessageAttachment -UserId $userPrincipalName -MessageId $message.Id
 
-                foreach ($attachment in $attachments.Value) {
+                foreach ($attachment in $attachments) {
                     $attachmentId = $attachment.Id
-                    $deleteAttachmentUri = "https://graph.microsoft.com/v1.0/users/$userPrincipalName/messages/$($message.Id)/attachments/$attachmentId"
-                    $attachmentSizeMB = [Math]::Round($attachment.size / 1000000, 3) 
+                    $attachmentSizeMB = [Math]::Round($attachment.Size / 1000000, 3) 
                     if($attachmentSizeMB -ge $minAttachmentSize){
                         try {
-                            Write-Output "Deleting attachment in Folder - '$($folder.displayName)', Email Subject - '$($message.subject)' from - '$($message.sender.Values.address)'"
-                            Invoke-MgGraphRequest -Method DELETE -Uri $deleteAttachmentUri -Headers $headers
+                            Write-Output "Deleting attachment in Folder - '$($folder.DisplayName)', Email Subject - '$($message.Subject)' from - '$($message.Sender.EmailAddress.Address)'"
+                            Remove-MgUserMessageAttachment -UserId $userPrincipalName -MessageId $message.Id -AttachmentId $attachmentId
     
                             # Add the email details to the Microsoft List
                             $listItemBody = @{
                                 fields = @{
-                                    Title = $message.sender.Values.name
-                                    Subject = $message.subject
+                                    Title = $message.Sender.EmailAddress.Name
+                                    Subject = $message.Subject
                                     AttachmentName = $attachment.Name 
                                     AttachmentSizeMB = $attachmentSizeMB
-                                    EmailCreatedDate = $message.createdDateTime.ToString("yyyy-MM-ddTHH:mm:ssZ")
-                                    FolderName = $folder.displayName
-                                    EmailURL = $message.webLink
+                                    EmailCreatedDate = $message.CreatedDateTime.ToString("yyyy-MM-ddTHH:mm:ssZ")
+                                    FolderName = $folder.DisplayName
+                                    EmailURL = $message.WebLink
+                                    ScriptRunDate = $todaysDate
                                     }
                                 }
-                                } catch {
-                                    Write-Error -Message $_.Exception
-                                    throw $_.Exception
-                                }
-                                Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0/sites/$siteId/lists/$($newListId.id)/items" -Body ($listItemBody | ConvertTo-Json) -Headers $headers -ContentType "application/json" | Out-Null
-                                Start-Sleep -Seconds 1
+                            New-MgSiteListItem -SiteId $siteId -ListId $newListId.Id -BodyParameter $listItemBody | Out-Null
+                            Start-Sleep -Seconds 1
+                        } catch {
+                            Write-Error -Message $_.Exception
+                            throw $_.Exception
+                        }
                     }                                       
                 }
             }
         }
 
-        if ($messages.Value.Count -lt $maxEmailsPerFolder) {
+        if ($messages.Count -lt $maxEmailsPerFolder) {
             break
         }
 
         $skipCount += $maxEmailsPerFolder
     }
 
-    Write-Output "Total messages processed in $($folder.displayName): $totalMessages"
+    Write-Output "Total messages processed in $($folder.DisplayName): $totalMessages"
 }
